682646201b4c8a06a70082d51312cf696f1d6461
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-21 17:01:37 +0100

    primeiros erros corrigidos

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerGoal == null)
+        {
+            Debug.LogWarning("Player Goal não atribuído ao EnemyX.");
+            return;
+        }
+
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = transform.position - other.gameObject.transform.position;
+
+            Debug.Log("Collided with " + other.gameObject.name + ", sending it away from the player.");
+
+            enemyRigidbody.AddForce(awayFromPlayer * speed, ForceMode.Impulse);
+        }
+    }
+}
